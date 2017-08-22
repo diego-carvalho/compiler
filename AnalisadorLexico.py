795b3fae30f9b0ac77e1f2f0f2c39c3a0d614a8a
Antonio <antoniomarciofilho@gmail.com>
2017-08-22 15:00:28 -0300

    Merdas do antonio

--- AnalisadorLexico.py
+++ AnalisadorLexico.py
@@ -1,10 +1,12 @@
 import sys
-
+import string
 
 from IO import IO
 from Token import Token
 
+
 dictionary = ['(', ')', '{', '}', ',', ';', '+', '-', '*', '/', ' ', '\n', '\t']
+words = ['int', 'main', 'float', 'if', 'else', 'while', 'read', 'print']
 tokens = []
 
 
@@ -102,6 +104,31 @@ def lexicalAnalyzer(char, file):
 		token = Token(char.char , "DIV", char.line )
 		addToken(token)
 
+    elif char.char in string.ascii_lowercase \
+            or char.char in string.ascii_uppercase:
+        newWord = char.char
+        while file.buffer[0].char in string.ascii_lowercase \
+                or file.buffer[0].char in string.ascii_uppercase \
+                or file.buffer[0].char in string.ascii_digits:
+            char = file.getNextChar()
+            newWord.__add__(char.char)
+        if newWord in words:
+            token = Token(char.char, str.upper(newWord), char.line ) 
+        else:
+            token = Token(char.char, "ID", char.line )
+        addToken(token)
+
+    elif char.char in string.digits:
+        while file.buffer[0].char in string.digits:
+            char = file.getNextChar()
+        if char.char == ".":
+            char = file.getNextChar()
+            while file.buffer[0].char in string.digits:
+                char = file.getNextChar()
+            token = Token(char.char, "FLOAT_CONST", char.line )
+        else:
+            token = Token(char.char, "INTEGER_CONST", char.line )
+        addToken(token)
 
 
 if __name__ == '__main__':
